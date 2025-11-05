4bda7d2f268c7be85382823d62c8f84b82fb21e6
HantangLiu123 <hantang.liu@mail.utoronto.ca>
2025-11-05 16:14:18 -0500

    fix: fixed the abnormal behavior of the mouse when moving left and down

--- Card-Flip/PS2_Controller/mouse.v
+++ Card-Flip/PS2_Controller/mouse.v
@@ -60,9 +60,9 @@ module mouse (clock, reset, mouse_x, mouse_y, left_button, PS2_CLK, PS2_DAT);
 			
 			if (current_s == recieve_0 && mouse_data_en && mouse_data[3])
 				packet_0 <= mouse_data;
-			else if (current_s == recieve_1 && mouse_data_en)
+			if (current_s == recieve_1 && mouse_data_en)
 				packet_1 <= mouse_data;
-			else if (current_s == recieve_2 && mouse_data_en)
+			if (current_s == recieve_2 && mouse_data_en)
 				packet_2 <= mouse_data;
 		end
 			
@@ -103,13 +103,14 @@ endmodule
 module cal_coor (packet_0, packet_1, packet_2, cur_x, cur_y, next_x, next_y, left_button);
 	input [7:0] packet_0, packet_1, packet_2;
 	input [9:0] cur_x;
-	input [9:0] cur_y;
+	input [8:0] cur_y;
 	output reg [9:0] next_x;
 	output reg [8:0] next_y;
 	output left_button;
 	
 	wire x_overflow, y_overflow, x_sign, y_sign;
 	wire [7:0] dx, dy;
+	reg signed [8:0] dx_signed, dy_signed;
 	
 	integer temp_x, temp_y;
 	
@@ -124,12 +125,15 @@ module cal_coor (packet_0, packet_1, packet_2, cur_x, cur_y, next_x, next_y, lef
 	always @(*) begin
 		temp_x = cur_x;
 		temp_y = cur_y;
+		
+		dx_signed = {x_sign, dx};
+		dy_signed = {y_sign, dy};
 
 		// x
 		if (x_overflow)
-			temp_x = cur_x + (x_sign ? -255 : 255);
+			temp_x = temp_x + (x_sign ? -255 : 255);
 		else
-			temp_x = cur_x + (x_sign ? -dx : dx);
+			temp_x = temp_x + dx_signed;
 
 		// clamp
 		if (temp_x < 0) temp_x = 0;
@@ -137,9 +141,9 @@ module cal_coor (packet_0, packet_1, packet_2, cur_x, cur_y, next_x, next_y, lef
 
 		// y
 		if (y_overflow)
-			temp_y = cur_y + (y_sign ? -255 : 255);
+			temp_y = temp_y + (y_sign ? 255 : -255);
 		else
-			temp_y = cur_y + (y_sign ? -dy : dy);
+			temp_y = temp_y - dy_signed;
 
 		if (temp_y < 0) temp_y = 0;
 		else if (temp_y > 479) temp_y = 479;
