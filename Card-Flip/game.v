0d2e0f8ca1d22661ab72d19810e76ae601513cd9
HantangLiu123 <hantang.liu@mail.utoronto.ca>
2025-11-16 22:20:47 -0500

    feat: in progress of adding the game fsm

--- Card-Flip/game.v
+++ Card-Flip/game.v
@@ -1,3 +1,85 @@
+module game (clock, resetn, game_start, rand_busy, mouse_x, mouse_y, left_button, show_busy,
+					card_num_reg, show_reg,
+					rand_start, load_card, resetn_show, resetn_timer, update_start, timer_start, 
+					timer_end, E_show, status_show, card_show);
+					
+	parameter nX = 10, nY = 9;
+					
+	input clock, resetn;
+	input game_start;
+	input rand_busy; // indicate if the random generator is still busy
+	input [nX-1:0] mouse_x;
+	input [nY-1:0] mouse_y;
+	input left_button;
+	input show_busy; // indicate if the screen is still in the update process
+	input [0:47] card_num_reg;
+	input [0:15] show_reg;
+	
+	output rand_start; // tell the random generator to start
+	output load_card; // tell the card register to load the card for this game
+	output resetn_show; // reset the show register
+	output resetn_timer; // reset the game timer
+	output update_start; // to start updating the screen
+	output timer_start, timer_end; // to start and end the game timer
+	output E_show; // enable the update of the show register
+	output status_show; // the status for updating the show register
+	output [3:0] card_show; // tell the show register that which card should be changed
+	
+	parameter idle = 5'd0, start_pressed = 5'd1, loading_card = 5'd2, update_card_1 = 5'd3;
+	parameter wait_update_1 = 5'd4, wait_click_1 = 5'd5, process_info_1 = 5'd6;
+	parameter update_card_2 = 5'd7, wait_update_2 = 5'd8, wait_click_2 = 5'd9;
+	parameter process_info_2 = 5'd10, update_card_3 = 5'd11, wait_update_3 = 5'd12;
+	parameter wait_1sec = 5'd13, flip_back_1 = 5'd14, flip_back_2 = 5'd15;
+	parameter update_card_4 = 5'd16, wait_update_4 = 5'd17, end_state = 5'd18;
+	
+	reg [4:0] current_s, next_s; // for states
+	
+	reg resetn_wait_cnt; // reset for the wait counter
+	reg E_wait_cnt; // enable the wait counter
+	wire done_wait_cnt; // done waiting
+	
+	reg [3:0] card1, card2; // the index of the cards flipped
+	wire [3:0] card_idx_out; // idx out of the mouse click
+	wire click_done;
+	
+	mouse_click MC (
+		.clock(clock),
+		.mouse_x(mouse_x),
+		.mouse_y(mouse_y),
+		.left_button(left_button),
+		.show(show_reg),
+		.card_idx_out(card_idx_out),
+		.click_done(click_done)
+	);
+	
+	cnt_wait CNT (
+		.clock(clock),
+		.resetn(resetn_wait_cnt),
+		.enable(E_wait_cnt),
+		.done(done_wait_cnt)
+	);
+	
+endmodule
+
+module cnt_wait (clock, resetn, enable, done);
+	// the counter for waiting for a second
+	input clock, resetn, enable;
+	output reg done;
+	
+	reg [25:0] Q;
+	always @(posedge clock) begin
+		done <= 1'b0;
+		if (!resetn) begin
+			Q <= 25'd0;
+		end else if (enable)
+			if (Q == 25'd50000000)
+				done <= 1'b1;
+			else
+				Q <= Q + 1;
+	end
+			
+endmodule
+
 module mouse_click (clock, mouse_x, mouse_y, left_button, show, card_idx_out, click_done);
 	parameter nX = 10;
 	parameter nY = 9;
